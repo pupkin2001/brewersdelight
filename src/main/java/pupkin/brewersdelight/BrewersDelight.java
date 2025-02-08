db7f42b68ce8b9155ad7bd33556cb6545948e2d2
pupkin2001 <131904062+pupkin2001@users.noreply.github.com>
2025-02-08 16:13:41 +0500

    Update 0.6

--- src/main/java/pupkin/brewersdelight/BrewersDelight.java
+++ src/main/java/pupkin/brewersdelight/BrewersDelight.java
@@ -1,9 +1,11 @@
 package pupkin.brewersdelight;
 
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
 import pupkin.brewersdelight.item.BrewersItems;
 import pupkin.brewersdelight.misc.BrewersDelightTab;
 
@@ -12,14 +14,16 @@ import pupkin.brewersdelight.misc.BrewersDelightTab;
 public class BrewersDelight
 {
 	public static final String MOD_ID = "brewersdelight";
-
+	
+	public static final Logger LOGGER = LogUtils.getLogger();
+	
 	public BrewersDelight()
 	{
 		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
+		
 		BrewersItems.register(eventBus);
 		BrewersDelightTab.CREATIVE_TABS.register(eventBus);
-
+		
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 }
