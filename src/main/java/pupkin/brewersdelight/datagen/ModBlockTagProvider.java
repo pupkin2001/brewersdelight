863d19bc2626c0cbf6481944716b8b9a6e097d8a
pupkin2001 <131904062+pupkin2001@users.noreply.github.com>
2025-03-25 16:42:11 +0500

    fix: Block tag provider referencing Farmers Delight

--- src/main/java/pupkin/brewersdelight/datagen/ModBlockTagProvider.java
+++ src/main/java/pupkin/brewersdelight/datagen/ModBlockTagProvider.java
@@ -5,14 +5,14 @@ import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
-import vectorwing.farmersdelight.FarmersDelight;
+import pupkin.brewersdelight.BrewersDelight;
 
 import javax.annotation.Nullable;
 import java.util.concurrent.CompletableFuture;
 
 public class ModBlockTagProvider extends BlockTagsProvider {
 	public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
-        super(output, lookupProvider, FarmersDelight.MODID, existingFileHelper);
+		super(output, lookupProvider, BrewersDelight.MOD_ID, existingFileHelper);
 	}
 	
 	@Override
